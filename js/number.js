048786bd7c76710253b41dfda658b3627b29ce19
Miguel Julián <figarocorso@gmail.com>
2023-12-16 23:37:41 +0100

    Refactor JS

---
+++ js/number.js
@@ -0,0 +1,16 @@
+function last(array) {
+  return array[array.length - 1];
+}
+
+function loadJSON(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', 'data.json', true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
+
