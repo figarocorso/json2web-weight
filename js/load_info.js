6079b254228e80e1a7d72c7156495c57f9ab051d
Miguel Julián <figarocorso@gmail.com>
2023-12-17 00:29:49 +0100

    Load data from external JSON file
    Now we need to develop using web server

--- js/load_info.js
+++ js/load_info.js
@@ -1,3 +1,13 @@
+function showData() {
+  var getUrl = window.location;
+  var baseUrl = getUrl.href;
+  var weightData = [];
+  $.getJSON(baseUrl + "data/weight.json").then(function (weightData) {
+    updateNumber(weightData);
+    renderChart(weightData);
+  });
+}
+
 function last(array) {
   return array[array.length - 1];
 }
@@ -32,22 +42,7 @@ function renderChart(data) {
   
   var chart = new Chart(ctx, config);
 }
+
 $( document ).ready(function() {
-  var getUrl = window.location;
-  var baseUrl = getUrl.href;
-  var weightData = [];
-  // $.getJSON(baseUrl + "data/weight.json").then(function (data) {
-  //     weightData = data;
-  // });
-
-  weightData = [
-    { "date": "2023-12-01", "weight": 116 },
-    { "date": "2023-12-02", "weight": 115 },
-    { "date": "2023-12-03", "weight": 110 },
-    { "date": "2023-12-04", "weight": 115 },
-    { "date": "2023-12-05", "weight": 112 }
-  ];
-
-  updateNumber(weightData);
-  renderChart(weightData);
+  showData();
 })
