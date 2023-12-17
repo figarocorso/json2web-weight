8824b43e30b5aef3ae6e50b0be2ac3ebb34a1087
Miguel Julián <figarocorso@gmail.com>
2023-12-17 01:11:12 +0100

    Add file from args and refactor

--- update_json.py
+++ update_json.py
@@ -1,43 +1,57 @@
-import datetime
 import argparse
+import datetime
 import json
 
-def get_data():
-    parser = argparse.ArgumentParser(description='Get weight and append JSON file.')
-    parser.add_argument('--weight', '-w', type=float, required=True, help='Floating-point or integer number')
-    args = parser.parse_args()
 
-    try:
-        weight = args.weight
+def get_args():
+    parser = argparse.ArgumentParser(description="Get weight and append JSON file.")
+    parser.add_argument(
+        "--weight",
+        "-w",
+        type=float,
+        required=True,
+        help="Floating-point or integer number",
+    )
+    parser.add_argument(
+        "--file",
+        "-f",
+        required=False,
+        default="data/weight.json",
+        help="JSON file to append the information",
+    )
+    return parser.parse_args()
 
-        if weight.is_integer():
-            weight = int(weight)
 
-        current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+def get_data(weight):
+    if weight.is_integer():
+        weight = int(weight)
 
-        new_data = {"weight": weight, "date": current_date}
-        return new_data
-    except ValueError:
-        print("Error: Enter a valid number (can be integer or floating-point).")
-        return None
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+    new_data = {"weight": weight, "date": current_date}
+
+    return new_data
+
 
 def update_json(new_data, file_path):
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             data_list = json.load(file)
     except FileNotFoundError:
         data_list = []
 
     data_list.append(new_data)
 
-    with open(file_path, 'w') as file:
+    with open(file_path, "w") as file:
         json.dump(data_list, file, indent=2)
 
+
 if __name__ == "__main__":
-    result_data = get_data()
+    args = get_args()
+    result_data = get_data(args.weight)
 
     if result_data:
-        json_file_path = "data/weight.json"
-        update_json(result_data, json_file_path)
-        print(f"The entered data '{result_data}' has been added to the file {json_file_path}.")
-
+        update_json(result_data, args.file)
+        print(
+            f"The entered data '{result_data}' has been added to the file {args.file}."
+        )
